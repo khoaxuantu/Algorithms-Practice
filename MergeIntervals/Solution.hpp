8eb0fc4f460aa282995a39652173fd517796a26d
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-01-12 00:27:26 +0700

    Modify merge intervals

--- MergeIntervals/Solution.hpp
+++ MergeIntervals/Solution.hpp
@@ -37,6 +37,45 @@ public:
 
 
 /**
+ * @brief Interval Intersection
+ * 
+ * @param arr1 The 1st interval list (disjoint intervals sorted on their start time)
+ * @param arr2 The 2nd interval list (  ""  ""  ""  ""  ""  ""  ""  ""  ""  ""  "" )
+ * 
+ * @return :vector<Interval>: The intersection between 2 lists
+ */
+class IntervalsIntersection
+{
+private:
+    vector<Interval> arr1;
+    vector<Interval> arr2;
+    bool isOverlap(Interval& x, Interval& y) {
+        if ((x.start <= y.start && x.end >= y.start) || 
+            (x.start >= y.start && x.start <= y.end)) return true;
+        return false;
+    }
+public:
+    IntervalsIntersection(vector<Interval>& arr1, vector<Interval>& arr2) :
+        arr1(arr1), arr2(arr2) {}
+    vector<Interval> solve() {
+        vector<Interval> ans;
+        int i = 0, j = 0;
+        Interval tmp(0,0);
+        while (i < arr1.size() && j < arr2.size()) {
+            if (isOverlap(arr1[i], arr2[j])) {
+                tmp.start = max(arr1[i].start, arr2[j].start);
+                tmp.end = min(arr1[i].end, arr2[j].end);
+                ans.push_back(tmp);
+            }
+            if (arr2[j].end > arr1[i].end) i++;
+            else j++;
+        }
+        return ans;
+    }
+};
+
+
+/**
  * @brief Employee Free Time 
  * 
  * @param schedule  A list of intervals representing the working hours
@@ -59,10 +98,10 @@ private:
         // Push the schedule of every employee into the heap
         for (auto employee : schedule)
         {
-        for (auto time : employee)
-        {
-            minHeap.push(time);
-        }
+            for (auto time : employee)
+            {
+                minHeap.push(time);
+            }
         }
         // Traverse the heap until it is empty
         // In each iter, pop out the top as the cur
@@ -70,13 +109,13 @@ private:
             // Push into the ans
         while (!minHeap.empty())
         {
-        Interval curTime = minHeap.top();
-        minHeap.pop();
+            Interval curTime = minHeap.top();
+            minHeap.pop();
 
-        if (curTime.end < minHeap.top().start)
-        {
-            ans.push_back({curTime.end, minHeap.top().start});
-        }
+            if (curTime.end < minHeap.top().start)
+            {
+                ans.push_back({curTime.end, minHeap.top().start});
+            }
         }
         return ans;
     }
--- MergeIntervals/main.cpp
+++ MergeIntervals/main.cpp
@@ -11,6 +11,15 @@ vector<vector<Interval>> MIIntervals {
   {{6, 7}, {2, 4}, {5, 9}},
   {{1, 4}, {2, 6}, {3, 5}}
 };
+//* Interval Intersection
+vector<vector<Interval>> IIArr1 {
+  {{1, 3}, {5, 6}, {7, 9}},
+  {{1, 3}, {5, 7}, {9, 12}}
+};
+vector<vector<Interval>> IIArr2 {
+  {{2, 3}, {5, 7}},
+  {{5, 10}}
+};
 //* Employee Free Time
 vector<vector<vector<Interval>>> EFTIntervals = {
   {{{1,3}, {5,6}}, {{2,3}, {6,8}}},
@@ -32,10 +41,12 @@ int main(int argc, char *argv[]) {
      * TODO: Modify input here
      * TODO: Print the output
      */
-    for (auto intervals : MIIntervals) {
-        MergeIntervals MI(intervals);
-        for (auto i : MI.solve()) {
-            cout << i.start <<"-"<<i.end <<" ";
+    for (int i = 0; i < IIArr1.size(); i++) {
+        IntervalsIntersection ii(IIArr1[i], IIArr2[i]);
+        for (auto interval : ii.solve()) {
+          cout << "[ ";
+          cout << interval.start << " " << interval.end << " ";
+          cout << "] ";
         }
         cout << endl;
     }
